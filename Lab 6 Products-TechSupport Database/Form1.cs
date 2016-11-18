661f41eee8e4c4df7d617c3720930a9cf25263dd
PaquinWyattA <PaquinWyattA@WYATT-PC>
2016-11-17 19:14:24 -0500

    Final Commit

---
+++ Lab 6 Products-TechSupport Database/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6_Products_TechSupport_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            try
+            {
+                this.productsBindingSource.EndEdit();
+            }
+            catch (NoNullAllowedException)
+            {
+                MessageBox.Show("Please enter valid data types!", "Database Error");
+            }
+            this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'techSupportDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.techSupportDataSet.Products);
+
+        }
+    }
+}
---
+++ Lab 6 Products-TechSupport Database/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6_Products_TechSupport_Database
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new Form1());
+            }
+            catch 
+            {
+                MessageBox.Show("You did something funky, don't do that!", "Database Error");
+            }
+        }
+
+
+    }
+}
